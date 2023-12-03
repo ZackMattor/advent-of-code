b0961f2b18a24789e59149431ead2b7b4a09d866
Zack Mattor <zachary.mattor@atlastravel.com>
2023-12-03 17:14:48 -0500

    Day 3 pt 2

--- challenges/2023/day_03/day_03.rb
+++ challenges/2023/day_03/day_03.rb
@@ -1,5 +1,6 @@
 class Day02 < Day
   def pt1
+    debug = false
     ans = 0
 
     @raw_lines.each_with_index do |line, y_loc|
@@ -33,17 +34,17 @@ class Day02 < Day
         # we should process the item.
         if (!was_num || x_loc == line.length-1) && !number_parts.empty?
           if sees_symbol
-            print(number_parts.join.colorize(:yellow))
+            print(number_parts.join.colorize(:yellow)) if debug
             ans = ans + number_parts.join.to_i
           else
-            print(number_parts.join.colorize(:white))
+            print(number_parts.join.colorize(:white)) if debug
           end
 
           number_parts = []
           sees_symbol = false
         end
 
-        print(char) unless char.match /\d/
+        print(char) unless char.match /\d/ if debug
       end
 
     end
@@ -52,6 +53,53 @@ class Day02 < Day
   end
 
   def pt2
-    nil
+    ans = 0
+    data = {}
+
+    @raw_lines.each_with_index do |line, y_loc|
+      number_parts = []
+      line_gear_locs = []
+
+      line.chars.each_with_index do |char, x_loc|
+        was_num = false
+
+        if char.match /\d/
+          was_num = true
+          number_parts.push(char)
+
+          # Check to see if this number
+          # observes a symbol
+          [
+            [1,0], [-1,0], [0,1], [0,-1],
+            [1,1], [-1,1], [1,-1], [-1,-1],
+          ].each do |vx, vy|
+            x = x_loc + vx
+            y = y_loc + vy
+
+            next if x < 0 || y < 0
+            next if @raw_lines[y]&.chars&.dig(x).nil?
+
+            if @raw_lines[y][x] == '*'
+              line_gear_locs.push([y,x])
+            end
+          end
+        end
+
+
+        # If this is the end of a number segment
+        # we should process the item.
+        if (!was_num || x_loc == line.length-1) && !number_parts.empty?
+          line_gear_locs.uniq.each do |loc|
+            data[loc] = [] if data[loc].nil?
+            data[loc].push(number_parts.join.to_i)
+          end
+
+          number_parts = []
+          line_gear_locs = []
+        end
+      end
+    end
+
+    data.select {|k,v| v.length == 2 }.map { |k,v| v.inject(:*)}.sum
   end
 end
